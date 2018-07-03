3acb017bf7827361fb6dcaeef616685fed5be76d
Nurudeen Sulaimon <san2datamastery@gmail.com>
2018-07-03 14:22:15 -0400

    Create Chap1_ML.py

---
+++ Chap1_ML.py
@@ -0,0 +1,70 @@
+# Load a CSV file
+def load_csv (filename):
+    file=open(filename,"r")
+    lines =reader(file)
+    dataset=list(lines)
+    return dataset
+
+
+# Load dataset
+filename = 'pima-indians-diabetics.csv'
+dataset = load_csv(filename)
+print ('Loaded data file{0} with {1} rows and {2} columns'.format(filename,len(dataset), len(dataset[0])))
+
+
+# Example of loading Pima Indians CSV dataset
+from csv import reader 
+# Load a csv file 
+def load_csv(filename):
+	dataset = list()
+	with open(filename,'r') as file:
+	csv_reader = reader(file)
+	for row in csv_reader:
+		if not row:
+			continue 
+		dataset.append(row)
+	return dataset
+
+
+# Convert string column to float 
+def str_column_to_float(dataset,column):
+	for row in dataset:
+		row[column]=float(row[column].strip())
+
+# Load pima-indians-diabetes dataset
+filename = 'pima-indian-diabetes.csv'
+dataset = load_csv(filename)
+print ('Loaded data file {0} with {1} rows and {2} columns'.format(filename, len(dataset), len(dataset[0])))
+print (dataset[0])
+#Convert string columns to float 
+for i in range(len(dataset[0])):
+	str_column_to_float(dataset,i)
+print (dataset[0])
+
+
+# Convert string to column to integer 
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value]=i
+	for row in dataset:
+		row[column]=lookup[row[column]]
+	return lookup
+
+# Load iris dataset
+filename ='iris.csv'
+dataset = load_csv(filename)
+print ('Loaded data file{0} with {1} rows  and {2} columns'.format(filename,len(dataset),len(dataset[0])))
+print(dataset[0])
+
+# convert string columns to float 
+for i in range(4):
+	str_column_to_float(dataset,i)
+# convert string columns to float
+lookup= str_column_to_int(dataset,4)
+print(dataset[0])
+print(lookup)
+
+
